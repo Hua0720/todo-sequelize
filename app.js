bfa4af1010c4d2bb9fdba0d6f0b4bc04795831d4
Hua0720 <stella573021@gmail.com>
2022-11-20 00:26:51 +0800

    add middlewarein app.js

--- app.js
+++ app.js
@@ -37,6 +37,22 @@ app.post('/users/login', passport.authenticate('local', {
 // 呼叫 Passport 函式並傳入 app
 usePassport(app)
 
+app.use(flash())  // 掛載套件
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  next()
+})
+
+// 加入一組 middleware
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
 // 設定路由
 app.get('/', (req, res) => {
   return Todo.findAll({
